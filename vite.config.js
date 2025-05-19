f4895fbe681a2dab1d7826f54f4c9b2d2e91767a
p3073845 <p3073845@gmail.com>
2025-05-19 11:40:02 +0530

    Configure CDN optimization

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
   resolve: {
     alias: {
       'jquery': 'jquery/dist/jquery.js',
